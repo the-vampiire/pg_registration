71ed10cf8e9f7fc1fdc0fd35b0f1e883d0a0db05
The-Vampiire <vampiirecodes@gmail.com>
2018-03-17 15:10:01 -0400

    Create Course Location table
    refactored directory naming pattern
    - mysite [project root / admin]
    - registration [registration app]
    
    added a Registration app
    - modularized models.py into a package of model classes
    
    created CourseLocation model
    - added validation pattern for international phone numbers
    - renamed table to "course_location"

--- princeton_groundwater/pg_site/wsgi.py
+++ princeton_groundwater/pg_site/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for pg_registration project.
+WSGI config for pg_site project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pg_registration.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pg_site.settings")
 
 application = get_wsgi_application()
