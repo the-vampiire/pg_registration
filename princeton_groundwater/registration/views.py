65e902b75e346a61ef908a1cbdcb294229d4f7cd
The-Vampiire <vampiirecodes@gmail.com>
2018-03-18 14:28:14 -0400

    Configured url patterns and created sample index view
    - routes all traffic to /register through the registration app
    - simple jinja based template view to make sure everything is wired up properly

--- princeton_groundwater/registration/views.py
+++ princeton_groundwater/registration/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404
+from .models import Course
 
-# Create your views here.
+def index(request):
+  context = { 'courses': get_list_or_404(Course) } # use GLO404 to handle empty query sets
+  return render(request, 'registration/index.html', context)
